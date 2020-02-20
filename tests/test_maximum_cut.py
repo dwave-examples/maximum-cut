8b559f72e81693a7b2951bf07beea4e027667d8d
Melody Wong <mwong@dwavesys.com>
2020-02-20 15:58:29 -0800

    Use virtual environment's python

--- tests/test_maximum_cut.py
+++ tests/test_maximum_cut.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import unittest
 
 # /path/to/demos/maximum-cut/tests/test_maximum_cut.py
@@ -11,4 +12,4 @@ class TestDemo(unittest.TestCase):
         """run maximum_cut.py and check that nothing crashes"""
 
         demo_file = os.path.join(project_dir, 'maximum_cut.py')
-        subprocess.check_output(["python", demo_file])
+        subprocess.check_output([sys.executable, demo_file])
